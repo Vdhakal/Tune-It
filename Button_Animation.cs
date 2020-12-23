88ab6a61b71240962d65d09e50860f384ef04372
Vaskar Dhakal <30727785+Vdhakal@users.noreply.github.com>
2020-12-23 13:10:04 -0600

    first

---
+++ Button_Animation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Animation : MonoBehaviour
+{
+    public Animation animation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = gameObject.GetComponent<Animation>();
+    }
+
+    public void button_touch()
+    {
+        // animation.Play("ButtonAnim");
+    }
+}
---
+++ Particle_System.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Particle_System : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem particleSystem1, particleSystem2;
+    GameObject selectedBtn;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ToggleParticles()
+    {
+        EventSystem currentEvent = EventSystem.current;
+        selectedBtn = currentEvent.currentSelectedGameObject;
+        particleSystem1.transform.position = new Vector3(selectedBtn.transform.position.x, selectedBtn.transform.position.y + Mathf.Abs(selectedBtn.transform.position.y), selectedBtn.transform.position.z);
+        particleSystem2.transform.position = new Vector3(selectedBtn.transform.position.x, selectedBtn.transform.position.y + Mathf.Abs(selectedBtn.transform.position.y), selectedBtn.transform.position.z);
+        particleSystem1.Play();
+        particleSystem2.Play();
+    }
+}
